b38e1c9343e793ec4a68095f94a0c9aa041ec1d2
quanhocdev <minhquanthcs257@gmail.com>
2025-06-25 11:23:46 +0700

    lam sach cus

--- demo/src/main/java/com/hao/demo/model/Customer.java
+++ demo/src/main/java/com/hao/demo/model/Customer.java
@@ -254,20 +254,6 @@ public class Customer {
         public String getPassword() { return password; }
         public void setPassword(String password) { this.password = password; }
 
-    // public String getPhone() { return phone; }
-    // public void setPhone(String phone) { this.phone = phone; }
-
-    // public String getAddress() { return address; }
-    // public void setAddress(String address) { this.address = address; }
-
-    // public String getDateOfBirth() { return dateOfBirth; }
-    // public void setDateOfBirth(String dateOfBirth) { this.dateOfBirth = dateOfBirth; }
-
-    // public String getGender() { return gender; }
-    // public void setGender(String gender) { this.gender = gender; }
-
-   // public LocalDateTime getCreatedAt() { return createdAt; }
-    // public LocalDateTime getUpdatedAt() { return updatedAt; }
     public Boolean getIsActive() { return isActive; }
     public void setIsActive(Boolean isActive) { this.isActive = isActive; }
 
