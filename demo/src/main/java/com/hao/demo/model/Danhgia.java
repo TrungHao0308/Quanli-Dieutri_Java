ca2e7c825d4fd1e303830049cb12714943e5de07
Nguyễn Trung Chí Hào <oha552005@gmail.com>
2025-07-02 20:08:50 +0700

    Update Danhgia.java
    chỉnh sửa lại model

--- demo/src/main/java/com/hao/demo/model/Danhgia.java
+++ demo/src/main/java/com/hao/demo/model/Danhgia.java
@@ -9,60 +9,34 @@ public class Danhgia {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String emailBacSi;
     private String tenBacSi;
     private String dichVu;
-    private int mucDoHaiLong; // 1 đến 5
+    private int mucDoHaiLong;
     private String nhanXet;
+    private String emailBenhNhan;
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @Column(nullable = false)
+    private boolean daXem = false;
 
-    public String getEmailBacSi() {
-        return emailBacSi;
-    }
-    public void setEmailBacSi(String emailBacSi) {
-        this.emailBacSi = emailBacSi;
-    }
-    public String getTenBacSi() {
-        return tenBacSi;
-    }
-    public void setTenBacSi(String tenBacSi) {
-        this.tenBacSi = tenBacSi;
-    }
-    public String getDichVu() {
-        return dichVu;
-    }
-    public void setDichVu(String dichVu) {
-        this.dichVu = dichVu;
-    }
+    // Getters & Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public int getMucDoHaiLong() {
-        return mucDoHaiLong;
-    }
-    public void setMucDoHaiLong(int mucDoHaiLong) {
-        this.mucDoHaiLong = mucDoHaiLong;
-    }
-    public String getNhanXet() {
-        return nhanXet;
-    }
-    public void setNhanXet(String nhanXet) {
-        this.nhanXet = nhanXet;
-    }
-    @Override
-    public String toString() {
-        return "Danhgia{" +
-                "id=" + id +
-                ", emailBacSi='" + emailBacSi + '\'' +
-                ", tenBacSi='" + tenBacSi + '\'' +
-                ", dichVu='" + dichVu + '\'' +
-                ", mucDoHaiLong=" + mucDoHaiLong +
-                ", nhanXet='" + nhanXet + '\'' +
-                '}';
-    }
+    public String getTenBacSi() { return tenBacSi; }
+    public void setTenBacSi(String tenBacSi) { this.tenBacSi = tenBacSi; }
+
+    public String getDichVu() { return dichVu; }
+    public void setDichVu(String dichVu) { this.dichVu = dichVu; }
+
+    public int getMucDoHaiLong() { return mucDoHaiLong; }
+    public void setMucDoHaiLong(int mucDoHaiLong) { this.mucDoHaiLong = mucDoHaiLong; }
+
+    public String getNhanXet() { return nhanXet; }
+    public void setNhanXet(String nhanXet) { this.nhanXet = nhanXet; }
+
+    public String getEmailBenhNhan() { return emailBenhNhan; }
+    public void setEmailBenhNhan(String emailBenhNhan) { this.emailBenhNhan = emailBenhNhan; }
+
+    public boolean isDaXem() { return daXem; }
+    public void setDaXem(boolean daXem) { this.daXem = daXem; }
 }
